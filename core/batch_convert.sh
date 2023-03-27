c9b20a234e5aa93f865ec13d57c6a5e42a4f8735
bg172 <https://github.com/bg172>
2023-03-27 23:13:05 +0200

    rename command-example script

---
+++ core/batch_convert.sh
@@ -0,0 +1,1 @@
+./gLAB -input:cfg meas.cfg -input:obs ../data/OBE400DEU_R_20223530000_01D_30S_MO.rnx --pre:prealign > L1L5obe.meas
--- core/convert.sh
+++
@@ -1,1 +0,0 @@
-./gLAB_linux -input:cfg meas.cfg -input:obs ../data/OBE400DEU_R_20223530000_01D_30S_MO.rnx --pre:prealign > L1L5obe.meas
